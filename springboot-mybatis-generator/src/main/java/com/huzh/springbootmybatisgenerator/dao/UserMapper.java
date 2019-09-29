c07e4c1b03c47b83413d315c2c7baa06f1d7011b
企鹅十五号 <251682553@qq.com>
2019-09-29 15:36:07 +0800

    spring boot 测试

--- springboot-mybatis-generator/src/main/java/com/huzh/springbootmybatisgenerator/dao/UserMapper.java
+++ springboot-mybatis-generator/src/main/java/com/huzh/springbootmybatisgenerator/dao/UserMapper.java
@@ -1,8 +1,11 @@
 package com.huzh.springbootmybatisgenerator.dao;
 
 import com.huzh.springbootmybatisgenerator.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
 import java.util.List;
 
+@Mapper
 public interface UserMapper {
     int deleteByPrimaryKey(Long id);
 
--- springboot-mybatis-generator/src/test/java/com/huzh/springbootmybatisgenerator/SpringbootMybatisGeneratorApplicationTests.java
+++ springboot-mybatis-generator/src/test/java/com/huzh/springbootmybatisgenerator/SpringbootMybatisGeneratorApplicationTests.java
@@ -1,7 +1,9 @@
 package com.huzh.springbootmybatisgenerator;
 
+import com.huzh.springbootmybatisgenerator.dao.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootMybatisGeneratorApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     public void contextLoads() {
+        System.out.println(userMapper.selectAll());
     }
 
 }
