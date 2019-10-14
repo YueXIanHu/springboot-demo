3254244ed3cee97c4e9f0e08a53afcb255c88f15
企鹅十五号 <251682553@qq.com>
2019-10-14 15:59:40 +0800

    spring boot 使用logback 日志

--- springboot-logback/src/main/java/com/huzh/springbootlogback/controller/IndexController.java
+++ springboot-logback/src/main/java/com/huzh/springbootlogback/controller/IndexController.java
@@ -25,8 +25,10 @@ public class IndexController {
      */
     @RequestMapping("/index")
     public String index() {
+        logger.trace("记录trace日志");
         logger.debug("记录debug日志");
         logger.info("访问了index方法");
+        logger.warn("记录了warn日志");
         logger.error("记录了error错误日志");
         return "index";
     }
---
+++ springboot-logback/src/main/java/com/huzh/springbootlogback/controller/IndexController2.java
@@ -0,0 +1,34 @@
+package com.huzh.springbootlogback.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName IndexController2
+ * @Description TODO
+ * @Date 2019/10/14 15:46
+ * @Author huzh
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+public class IndexController2 {
+
+    @RequestMapping("/index2")
+    public String index() {
+        log.trace("记录trace日志");
+        log.debug("记录debug日志");
+        log.info("访问了index方法");
+        log.warn("记录了warn日志");
+        log.error("记录了error错误日志");
+
+        try {
+            int i = 0;
+            int j = 1 / i;
+        } catch (Exception e) {
+            log.error("错误异常：", e);
+        }
+        return "index";
+    }
+}
